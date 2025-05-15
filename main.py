aeddc3fef64c4fa9c76c6305927a491e9260502f
Gil Sharon <ygilsharon@gmail.com>
2025-05-15 23:02:56 +0300

    Refined blink detection, added other metrics

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from muses_project.graph_eeg import graph_eeg
-from muses_project.fork_muse_stream import fork_muse_stream
 
 if __name__ == "__main__":
     graph_eeg()
--- muses_project/graph_eeg.py
+++ muses_project/graph_eeg.py
@@ -1,12 +1,18 @@
-from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from muselsl.stream import list_muses
+from muselsl.muse import Muse
+from functools import partial
 from playsound import playsound
-
+from keyboard import on_press
+import asyncio
 import numpy as np
+from scipy.signal import butter, lfilter
+from os import path, getcwd
 import time
-MAX_MEMORY = 100
-
+MAX_MEMORY = 2000
+EEG_CHANNELS = 4
+ACC_CHANNELS = GYRO_CHANNELS = 3
 
 def compute_band_powers(eegdata, fs, band:int=1):
     """Extract the features (band powers) from the EEG.
@@ -63,79 +69,112 @@ class MuseController:
         self.shift_length = self.epoch_length - self.overlap_length
         self.band_powers = np.ones((4, 1))
         self.cooldown_start = 0
-        self.inlet = None
         self.time_correction = None
-        self.s_rate = None
+        self.eeg_sample_rate = 256
         self.eeg_buffer = None
+        self.eeg_arr = np.zeros(shape=(1,EEG_CHANNELS))
+        self.acc_arr = np.zeros(shape=(1,ACC_CHANNELS))
+        self.gyro_arr = np.zeros(shape=(1,GYRO_CHANNELS))
+        self.switching = False
+        self.on = False
+        self.muse = None
         self.done = False
 
-    def connect_to_streams(self):
-        print("Connecting to muse-s stream...")
-        streams = resolve_byprop('type', 'EEG', timeout=2)
-        if len(streams) == 0:
-            print("No muses found!")
-            return 1
-        self.inlet = StreamInlet(streams[0], max_chunklen=12)
-        self.s_rate = int(self.inlet.info().nominal_srate())
-        self.eeg_buffer = np.ones((self.s_rate * self.buffer_length, 4))
+    def connect_to_stream(self):
+        muses = list_muses()
+        address = muses[0]["address"]
+
+        def push(data, _, metric_type):
+            arr = getattr(self, metric_type + "_arr")
+            # :4 because eeg has a redundant channel
+            arr = np.concatenate((arr,  data.T[:, :4]))
+            if arr.shape[0] > MAX_MEMORY:
+                arr = arr[arr.shape[0] - MAX_MEMORY:]
+            setattr(self, metric_type + "_arr", arr)
+
+        push_eeg = partial(push, metric_type="eeg")
+        push_acc = partial(push, metric_type="acc")
+        push_gyro = partial(push, metric_type="gyro")
+
+        self.muse = Muse(address=address, callback_eeg=push_eeg, callback_acc=push_acc,
+                    callback_gyro=push_gyro)
+
+        connected = self.muse.connect(retries=1)
+        if connected:
+            self.muse.start()
+
+        self.eeg_buffer = np.ones((self.eeg_sample_rate * self.buffer_length, 4))
         return 0
 
-    def acquire_data(self):
-        eeg_data, timestamp = self.inlet.pull_chunk(timeout=1, max_samples=int(self.shift_length * self.s_rate))
-        eeg_data = np.array(eeg_data)[:,:4]
-        cutoff = self.eeg_buffer.shape[0] - eeg_data.shape[0]
-        self.eeg_buffer = np.concatenate((eeg_data, self.eeg_buffer[:cutoff]), axis=0)
-        epoch = self.eeg_buffer[:int(self.epoch_length * self.s_rate)]
-        band_powers = compute_band_powers(epoch, self.s_rate)[:, np.newaxis]
-        self.band_powers = np.concatenate((self.band_powers, band_powers), axis=1)
-        self.band_powers = self.band_powers[:, -MAX_MEMORY:]
+
+def butter_lowpass(cutoff, fs, order=5) -> (float, float):
+    return butter(order, cutoff, fs=fs, btype="low", analog=False)
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    return lfilter(b, a, data)
+
 
 def graph_eeg():
     controller = MuseController()
-    if controller.connect_to_streams() == 0:
+    if controller.connect_to_stream() == 0:
         t = np.linspace(1, MAX_MEMORY, MAX_MEMORY)
-        fig, axes = plt.subplots(nrows=2, ncols=2)
+        fig, axes = plt.subplots(nrows=3, ncols=4)
         lines = []
-        channels = ["left ear", "left temple", "right temple", "right ear"]
+        channels = ["Left Ear", "Left Temple", "Right Temple", "Right Ear", "Accelerometer X", "Accelerometer Y",
+                    "Accelerometer Z", "Gyroscope X", "Gyroscope Y", "Gyroscope Z"]
         colors = ["blue", "orange", "green", "red"]
-        for i, ax in enumerate(axes.flat):
-            line, = ax.plot([], [], color=colors[i])  # Start with empty data
-            ax.set_title(f"Theta - {channels[i]}")
+        for i in range(10):
+            ax = axes.flat[i]
+            line, = ax.plot([], [], color=colors[i % 4])
+            ax.set_title(channels[i])
             ax.set_xlim([0, MAX_MEMORY])
-            ax.set_ylim([-3, 6])  # Use set_ylim instead of duplicate set_xlim
+            if i < 4:
+                ax.set_ylim([-1000, 1000])
+            elif i < 7:
+                ax.set_ylim([-5, 5])
+            else:
+                ax.set_ylim([-200, 200])
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
             lines.append(line)
+        fig.tight_layout()
 
+        def blink_detection():
+            # Crude Strong Blink Detection
+            right_ear_var = np.var(controller.eeg_arr[-350:][:,0])
+            left_ear_var = np.var(controller.eeg_arr[-350:][:,3])
+            if not controller.switching:
+                if right_ear_var > 50000 and left_ear_var > 50000:
+                    controller.switching = True
+                    controller.on = ~controller.on
+                    if controller.on:
+                        playsound(r"assets\sounds\turn_on_beep.wav")
+                    else:
+                        playsound(r"assets\sounds\turn_off_beep.wav")
+            elif right_ear_var < 30000 and left_ear_var < 30000:
+                print(right_ear_var)
+                controller.switching = False
 
-        def update(frame):
-            controller.acquire_data()
+        def update(_):
+            for j in range(4):
+                lines[j].set_data(t[:controller.eeg_arr.shape[0]], butter_lowpass_filter(controller.eeg_arr[:, j],
+                                                                                         16, 256, 5))
+            for j in range(4, 7):
+                lines[j].set_data(t[:controller.acc_arr.shape[0]], controller.acc_arr[:, j - 4])
+            for j in range(7, 10):
+                lines[j].set_data(t[:controller.gyro_arr.shape[0]],controller.gyro_arr[:, j - 7])
 
-            # Handle cooldown logic
-            if controller.cooldown_start == 0:
-                if controller.band_powers[0].shape[0] > 10:
-                    high_theta_count = 0
-                    for i in range(10):
-                        if controller.band_powers[1][-i] > 1.15:
-                            high_theta_count += 1
-                    if high_theta_count > 5:
-                        # playsound("assets\\sounds\\vine-boom.mp3")
-                        controller.cooldown_start = time.time()
-            elif time.time() - controller.cooldown_start >= 1:
-                controller.cooldown_start = 0
+            blink_detection()
 
-            # Get the actual data length (up to MAX_MEMORY points)
-            data_len = min(controller.band_powers.shape[1], MAX_MEMORY)
-
-            # Use only the most recent data points
-            data_start = max(0, controller.band_powers.shape[1] - MAX_MEMORY)
-            data_end = controller.band_powers.shape[1]
-
-            # Update each line with the data
-            for i, line in enumerate(lines):
-                lines[i].set_data(t[:data_len], controller.band_powers[i, data_start:data_end])
+            # Wait for more data.
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(asyncio.sleep(0.1))
 
             return lines
 
         ani = animation.FuncAnimation(fig, update, cache_frame_data=False, interval=100, blit=True)
         plt.show()
+        controller.muse.stop()
+        controller.muse.disconnect()
